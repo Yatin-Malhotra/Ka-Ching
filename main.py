8abbe0f0888ea4885a66ff1b7c2c191b255cc1f8
Yatin <127234342+Yatin-Malhotra@users.noreply.github.com>
2023-05-20 13:24:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ storage_file = "bank_accounts.json"
 bank = Bank(storage_file)
 bank.create_account("1234567890", initial_balance=1000)
 bank.deposit("1234567890", 500)
-bank.withdraw("1234567890", 200)
+bank.withdraw("1234567890", 200, "Expense")
 bank.transfer("1234567890", "9876543210", 300, tag="Expense")
 bank.lend("1234567890", "8765432109", 1000, 0.05, tag="Loan")
 print(bank.get_balance("1234567890"))
